11d63bb21a40da31685cb3df0fb79617027d7869
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-19 14:15:46 -0300

    feat: route to callback of authentication with github

--- start/routes.ts
+++ start/routes.ts
@@ -1,9 +1,14 @@
-import { Router } from 'express'
+import { Router, Request, Response } from 'express'
 
 const router = Router()
 
 router.get('/github', (request: Request, response: Response) => {
   return response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+router.get('/signin/callback', (request: Request, response: Response) => {
+  const { code } = request.query
+  return response.json(code)
 })
 
 export default router
