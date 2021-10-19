df254e622a3c608e8653d827129d23f860daefe4
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-19 14:26:47 -0300

    typing access_token and user

--- services/AuthenticateUserService.ts
+++ services/AuthenticateUserService.ts
@@ -2,6 +2,18 @@ import axios from 'axios'
 import PrismaClient from '@prisma/client'
 import { sign } from 'jsonwebtoken'
 
+/* ------| Types |------ */
+interface IAccessTokenResponse {
+  access_token: string
+}
+
+interface IUserResponse {
+  id: number
+  name: string
+  login: string
+  avatar_url: string
+}
+
 export class AuthenticateUserService {
   async execute(code: string) {}
 }
