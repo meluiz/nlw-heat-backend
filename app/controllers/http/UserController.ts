a5d3d4867dff678b232ab4f8b09ab7466ec3be90
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-20 01:09:25 -0300

    feat: getting/registering users

--- app/controllers/http/UserController.ts
+++ app/controllers/http/UserController.ts
@@ -5,5 +5,15 @@ import { AuthenticateUserService } from '@service/AuthenticateUserService'
 
 export class UserController {
   async handle(request: Request, response: Response) {
+    const { code } = request.body
+    const service = new AuthenticateUserService()
+    
+    try {
+      const result = await service.execute(code)
+      return response.json(result)
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
+  
   }
 }
