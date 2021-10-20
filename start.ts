26b5eecf54bd66f6554921db6c813c1a900a2e09
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-20 01:31:46 -0300

    formatting log on server ready

--- start.ts
+++ start.ts
@@ -1,3 +1,4 @@
+import tynt  from 'tynt'
 import express from 'express'
 import dotenv from 'dotenv'
 
@@ -9,5 +10,18 @@ const application = express()
 application.use(express.json())
 application.use(router)
 
-application.listen(process.env.PORT, () => console.log(`🎉 Woow! Server is running on PORT ${process.env.PORT}`))
+application.listen(process.env.PORT, () => {
+  const log = `
+${tynt.Green('Server is running')}
+
+This server is running on ${tynt.Blue(process.env.PORT)}
+
+  Local:               https://localhost:${process.env.PORT}
+  With url like ip:    https://127.0.0.1:${process.env.PORT}
+
+Note: Project was make by meluiz.com
+`
+  console.clear()
+  console.log(log)
+})
 
