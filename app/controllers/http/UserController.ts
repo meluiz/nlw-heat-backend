d2fbe2dba82e1c8c52937deeadb0c0762ed560ca
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-20 01:07:29 -0300

    importing the AuthenticateUserService service

--- app/controllers/http/UserController.ts
+++ app/controllers/http/UserController.ts
@@ -1,5 +1,8 @@
 import { Request, Response } from 'express'
 
+/* ------| Services |------ */
+import { AuthenticateUserService } from '@service/AuthenticateUserService'
+
 export class UserController {
   async handle(request: Request, response: Response) {
   }
