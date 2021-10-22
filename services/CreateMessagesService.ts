f3a2b1b478186bc8796d6da27f17957961ddf823
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-22 00:34:45 -0300

    emitted data with new messages

--- services/CreateMessagesService.ts
+++ services/CreateMessagesService.ts
@@ -1,4 +1,5 @@
 import PrismaClient from "@prismajs/client";
+import { io } from "server";
 
 export class CreateMessagesService {
   async execute(message: string, user_id: string) {
@@ -12,6 +13,19 @@ export class CreateMessagesService {
         }
     })
 
-    return message
+    const data = {
+      message: m.message,
+      user_id: m.user_id,
+      user: {
+        avatar: m.user.avatar_url,
+        username: m.user.name,
+        name: m.user.name,
+      },
+      createdAt: m.created_at
+    }
+
+    io.emit('new_message', data)
+
+    return m
   }
 }
