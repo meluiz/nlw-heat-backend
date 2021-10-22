9c78e33c4b82a8abbd222a70a8f8ea2a1517efc3
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-22 00:51:45 -0300

    feat: controller to get last 3 messages

--- app/controllers/http/MessageController.ts
+++ app/controllers/http/MessageController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 /* ------| Services |------ */
 import { CreateMessagesService } from '@service/CreateMessagesService'
+import { GetLastThreeMessagesService } from '@service/GetLastThreeMessagesService'
 
 export class MessageController {
   async handle(request: Request, response: Response) {
@@ -12,6 +13,16 @@ export class MessageController {
 
     try {
       const result = await service.execute(message, user_id)
+      return response.json(result)
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
+  }
+  
+  async GetLastThreeMessages(request: Request, response: Response) {
+    const service = new GetLastThreeMessagesService()
+    try {
+      const result = await service.execute()
       return response.json(result)
     } catch (error) {
       return response.json({ error: error.message })
