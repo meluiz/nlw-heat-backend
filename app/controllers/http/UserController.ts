87e1aefd7513d27e0fc4592b497a5a65ed76d7e8
Luiz Felipe <52682525+meluiz@users.noreply.github.com>
2021-10-22 00:52:05 -0300

    feat: controller to get user

--- app/controllers/http/UserController.ts
+++ app/controllers/http/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 
 /* ------| Services |------ */
 import { AuthenticateUserService } from '@service/AuthenticateUserService'
+import { ProfileUserService } from '@service/ProfileUserService'
 
 export class UserController {
   async handle(request: Request, response: Response) {
@@ -15,5 +16,17 @@ export class UserController {
       return response.json({ error: error.message })
     }
   
+  }
+
+  async profile(request: Request, response: Response) {
+    const { user_id } = request
+    const service = new ProfileUserService()
+    
+    try {
+      const result = await service.execute(user_id)
+      return response.json(result)
+    } catch (error) {
+      return response.json({ error: error.message })
+    }
   }
 }
